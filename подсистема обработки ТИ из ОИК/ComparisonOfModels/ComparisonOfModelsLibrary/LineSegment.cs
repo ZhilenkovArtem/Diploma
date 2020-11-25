132881511b6cf5af3e6df36a3490cfdd17d1332e
Zhilenkov Artem <zhilenkovartem@mail.ru>
2020-11-25 15:50:44 +0700

    added Opisanie v xlsx and Reshenie na C#

---
+++ подсистема обработки ТИ из ОИК/ComparisonOfModels/ComparisonOfModelsLibrary/LineSegment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonOfModelsLibrary
+{
+    public class LineSegment
+    {
+        public bool State { get; set; }
+
+        public float ActivePowerAtFirst { get; set; }
+
+        public float ActivePowerInTheEnd { get; set; }
+
+        public float ReactivePowerAtFirst { get; set; }
+
+        public float ReactivePowerInTheEnd { get; set; }
+
+        public float CurrentAtFirst { get; set; }
+
+        public float CurrentInTheEnd { get; set; }
+
+        public float BaseVoltage { get; set; }
+
+        public int Address { get; set; }
+    }
+}
---
+++ подсистема обработки ТИ из ОИК/ComparisonOfModels/ComparisonOfModelsLibrary/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonOfModelsLibrary
+{
+    public class Node
+    {
+        public bool State { get; set; }
+
+        public float Voltage { get; set; }
+
+        public float BaseVoltage { get; set; }
+
+        public int Address { get; set; }
+    }
+}
